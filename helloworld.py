311cc2dde2a44b5ac9f8fc41e793ce7f9891cbb6
zukonarya1 <zukonarya+github@gmail.com>
2025-09-01 15:54:39 -0700

    added # Working with Conditionals

--- helloworld.py
+++ helloworld.py
@@ -57,7 +57,7 @@ print(myFavoriteFruitDictionary["Paulo"])
 myMixedTypeList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
 for item in myMixedTypeList:
     print("{} is of the data type {}".format(item,type(item)))
-'''
+
 import csv
 import copy
 myVehicle = {
@@ -73,3 +73,25 @@ myVehicle = {
 for key, value in myVehicle.items():
     print("{} : {}".format(key,value))
 myInventoryList = []
+'''
+
+
+# Working with Conditionals
+
+userReply = input("Do you need to ship a package? (Enter yes or no) ")
+if userReply == "yes":
+    print("We can help you ship that package!")
+
+else:
+    print("Please come back when you need to ship a package. Thank you.")
+
+userReply = input("Would you like to buy stamps, buy an envelope, or make a copy? (Enter stamps, envelope, or copy) ")
+if userReply == "stamps":
+    print("We have many stamp designs to choose from.")
+elif userReply == "envelope":
+    print("We have many envelope sizes to choose from.")
+elif userReply == "copy":
+    copies = input("How many copies would you like? (Enter a number) ")
+    print("Here are {} copies.".format(copies))
+else:
+    print("Thank you, please come again.")
